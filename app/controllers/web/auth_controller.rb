4e5f7328aba5024b78f18b7989b68cfadff3d69f
yorickov <yorickov@gmail.com>
2021-11-12 23:21:23 +0300

    Fix auth

--- app/controllers/web/auth_controller.rb
+++ app/controllers/web/auth_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class Web::AuthController < Web::ApplicationController
+  skip_before_action :verify_authenticity_token
+
   def github
     user = User.from_auth(request.env['omniauth.auth'])
     sign_in(user)
